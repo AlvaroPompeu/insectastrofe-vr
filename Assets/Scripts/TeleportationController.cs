3d13117ed7b25301a8ed64edf7c114424074d9fd
Alvaro Pompeu <alvaro.pompeu@gruposifra.com.br>
2022-04-22 14:25:00 -0300

    Teleportation areas and controller tweaks

--- Assets/Scripts/TeleportationController.cs
+++ Assets/Scripts/TeleportationController.cs
@@ -6,10 +6,18 @@ using UnityEngine.Events;
 
 public class TeleportationController : MonoBehaviour
 {
+    [SerializeField] Transform XROrigin;
+    [SerializeField] FadeCanvas fadeCanvas;
     [SerializeField] InputActionReference teleportActivationReference;
     [Space]
     [SerializeField] UnityEvent onTeleportActivate;
     [SerializeField] UnityEvent onTeleportCancel;
+
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    private bool teleportActivated = false;
+    private bool teleportReady = true;
+    private float teleportCooldown = 0.5f;
 
     private void Start()
     {
@@ -19,16 +27,41 @@ public class TeleportationController : MonoBehaviour
 
     private void TeleportModeCancel(InputAction.CallbackContext obj)
     {
-        Invoke("DeactivateTeleporter", 0.1f);
+        Invoke("DeactivateTeleporter", 0.03f);
     }
 
     private void DeactivateTeleporter()
     {
         onTeleportCancel.Invoke();
+        targetPosition = XROrigin.position;
+        TeleportHandler();
     }
 
     private void TeleportModeActivate(InputAction.CallbackContext obj)
     {
-        onTeleportActivate.Invoke();
+        if (teleportReady)
+        {
+            onTeleportActivate.Invoke();
+            originalPosition = XROrigin.position;
+            teleportActivated = true;
+        }
+    }
+
+    // Check if the player position has changed and fade if so, also manage the teleport cooldown
+    private void TeleportHandler()
+    {
+        if (originalPosition != targetPosition && teleportActivated)
+        {
+            fadeCanvas.Fade();
+            teleportActivated = false;
+            teleportReady = false;
+            StartCoroutine(TeleportCooldown());
+        }
+    }
+
+    IEnumerator TeleportCooldown()
+    {
+        yield return new WaitForSeconds(teleportCooldown);
+        teleportReady = true;
     }
 }
